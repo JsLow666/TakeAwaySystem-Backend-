0685d66bfcaf92854e59ceb2d9525acd2b68aa94
lowjunsheng <l.junsheng0528@gmail.com>
2025-09-30 22:55:24 +0800

    购物车（添加，查看，清空）代码开发

--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,5 +1,6 @@
 package com.sky.mapper;
 
+import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
@@ -18,5 +19,4 @@ public interface SetmealDishMapper {
 
     @Select("select * from setmeal_dish where setmeal_id = #{setmealId}")
     List<SetmealDish> getBySetmealId(Long id);
-
 }
